9c91d7cdcd98653f9236f2ea47c9a5aa32122410
mohamadahmadi1990 <m.a.yourdesigner@gmail.com>
2026-05-02 13:13:46 -0400

    Force dynamic admin requests page

--- src/app/admin/requests/page.tsx
+++ src/app/admin/requests/page.tsx
@@ -1,5 +1,8 @@
 import { getAppointmentRequests } from "@/db";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function AdminRequestsPage() {
   const requests = await getAppointmentRequests();
 
